b6fb9a10b780e2b533647312586c93d8cb1a869c
radhot <micheal8060@gmail.com>
2023-11-27 14:36:39 +0900

    1日目

---
+++ species2.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css"/>
+    <link rel="stylesheet" href="./index.css">
+</head>
+<body>
+    <header>
+        <h1>ペンギン図鑑</h1>
+    </header>
+    <main>
+        <aside>
+            <h3>コウテイペンギン属</h3>
+            <ul>
+                <li>コウテイペンギン</li>
+                <li>オウサマペンギン</li>
+            </ul>
+            <h3>アデリーペンギン属</h3>
+            <ul>
+                <li>アデリーペンギン</li>
+                <li>ジェンツーペンギン</li>
+                <li>ヒゲペンギン</li>
+            </ul>
+            <h3>フンボルトペンギン属</h3>
+            <ul>
+                <li>フンボルトペンギン</li>
+                <li>マゼランペンギン</li>
+                <li>ケープペンギン</li>
+                <li>ガラパゴスペンギン</li>
+            </ul>
+            <h3>マカロニペンギン属</h3>
+            <ul>
+                <li>イワトビペンギン</li>
+                <li>マカロニペンギン</li>
+                <li>ロイヤルペンギン</li>
+                <li>フィヨルドランドペンギン</li>
+                <li>シュレーターペンギン</li>
+                <li>スネアーズペンギン</li>
+            </ul>
+            <h3>キンメペンギン属</h3>
+            <ul>
+                <li>キンメペンギン</li>
+            </ul>
+            <h3>コガタペンギン属</h3>
+            <ul>
+                <li>コガタペンギン</li>
+                <li>ハネジロペンギン</li>
+            </ul>
+        </aside>
+        <div>
+            <h2>コウテイペンギン</h2>
+            <img src="./img/penguin01.jpg" alt="">
+            <p>コウテイペンギンは南極大陸にのみ生息するペンギンで、体長100～130㎝、体重20～45kgになる現存するペンギンの中で世界最大のペンギンです。<br>
+                雛鳥はよくアニメなどのキャラクターに使用されることが多く、人気が高いので比較的に日本ではなじみ深い印象ですが、日本では愛知県の名古屋港水族館と和歌山県の南紀白浜アドベンチャーワールドの2か所でしか飼育されておらず、繁殖に成功しているのはアドベンチャーワールドのみです。
+                繁殖期は冬で海から離れた内陸部で集団で繁殖します。また、泳ぎに関しては鳥類の中では最も潜水能力が高く、水深500m以上の深海にも潜ることができます。
+                餌はオキアミや小魚などを主食とし、天敵はシャチやヒョウアザラシ、オオフルマカモメなどがいます。</p>
+        </div>
+    </main>
+
+</body>
+</html>
